83bd56530b4bfda3301167a094c49e23245a33ec
Alex Soper <alexsoper29@gmail.com>
2014-11-16 17:18:44 -0500

    Adding in some JS

---
+++ _includes/scripts.js
@@ -0,0 +1,5 @@
+$(window).scroll(function(i){
+    var scrollVar = $(window).scrollTop();
+    $('#head-img').css({'top': 0.7*scrollVar });
+    $('#head-img').css({'opacity':( 100-scrollVar )/100});
+});
