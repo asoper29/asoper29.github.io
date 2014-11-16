eb92ca495bee5dc6037c2918669dfd86efcb3b02
Alex Soper <alexsoper29@gmail.com>
2014-11-16 17:20:44 -0500

    JS file in wrong spot

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(window).scroll(function(i){
+$(window).scroll(function(){
     var scrollVar = $(window).scrollTop();
     $('#head-img').css({'top': 0.7*scrollVar });
     $('#head-img').css({'opacity':( 100-scrollVar )/100});
